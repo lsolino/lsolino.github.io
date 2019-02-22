ca1658f03ebe820536ee3f458e233870a97eea94
Luan Solino <luansolino@gmail.com>
2019-02-22 15:17:11 -0300

    JS 1

---
+++ js1/idade.js
@@ -0,0 +1,27 @@
+function idade(dia, mes,ano) {
+    var data_atual = new Date,
+        ano_atual = data_atual.getFullYear(),
+        mes_atual = data_atual.getMonth() + 1,
+        dia_atual = data_atual.getDate(),
+
+        dia = +dia,
+        mes = +mes,
+        ano = +ano,
+        
+        idade = ano_atual - ano;
+
+    if (mes_atual < mes || mes_atual == mes && dia_atual < dia) {
+        idade--;
+    }
+ 
+    return idade;
+}
+
+var dia,
+    mes,
+    ano;
+
+dia = prompt('Digite o dia de seu nascimento: ');
+mes = prompt('Digite o mes de seu nascimento: ');
+ano = prompt('Digite o ano de seu nascimento: ');
+alert(idade(dia, mes, ano));
---
+++ js1/palindrome.js
@@ -0,0 +1,17 @@
+
+function palindrome(palavra) {     
+  for(var i = 0; i < palavra.length / 2; i++){
+    if (palavra[i] != palavra.length - i - 1]) {
+      return false;
+    } else {
+      return true;
+    }
+  }  
+}
+
+var palavra = prompt('Digite algo: ');
+if (palindrome(palavra) === true){
+  alert("É palindrome");
+} else {
+  alert("Não é palindrome");
+}
